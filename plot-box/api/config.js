c3e419a52c3cdc61d910e70e351ee77897fff024
lukej <31764152+sexton-luke@users.noreply.github.com>
2022-04-03 15:21:02 +1000

    add config file for global variables

---
+++ plot-box/api/config.js
@@ -0,0 +1,28 @@
+// config file for constant variables
+
+module.exports = {
+  //api
+  ENDPOINT: '/api/coordinates',
+
+  // express
+  JSON_OPTIONS: [
+    {
+      strict: true, // only accepts arrays and objects
+      limit: '1000kb', // limit payload to 1mb
+    },
+  ],
+
+  // validator request fields
+  NORTH_EAST_BOUNDARY: 'northEastBoundary',
+  SOUTH_WEST_BOUNDARY: 'southWestBoundary',
+
+  // default localhost port if environment variable does not exist
+  PORT: 3000,
+
+  // generate random number params
+  CEILING: 10,
+  OFFSET: 1,
+
+  // request codes - error handling
+  BAD_REQUEST: 400,
+};
