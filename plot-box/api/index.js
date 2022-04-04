9632a59c20d86806572115af529ec05df187383b
lukej <31764152+sexton-luke@users.noreply.github.com>
2022-04-04 14:16:39 +1000

    create and send coordinates in JSON response

--- plot-box/api/index.js
+++ plot-box/api/index.js
@@ -11,8 +11,8 @@ app.use(express.json(config.JSON_OPTIONS)); // Allow JSON payloads
 
 app.get(
   config.ENDPOINT,
-  body(config.NORTH_EAST_BOUNDARY).isArray,
-  body(config.SOUTH_WEST_BOUNDARY).isArray,
+  body(config.NORTH_EAST_BOUNDARY).isArray(),
+  body(config.SOUTH_WEST_BOUNDARY).isArray(),
   (req, res) => {
     // handle validation errors from request
     const errors = validationResult(req);
@@ -20,14 +20,75 @@ app.get(
       return res.status(config.BAD_REQUEST).json({ errors: errors.array() });
     }
 
+    // create latitude and longitude boundaries
+    const [latitudeBoundaries, longitudeBoundaries] =
+      createLatitudeLongitudeArrays(req.body);
+
     // get random number between 1 and 10
     const randomNumber = getRandomNumber(config.CEILING, config.OFFSET);
-    // const coordinates = getRandomCoordinates(randomNumber);
+
+    // get random coordinates between latitude/longitude boundaries
+    const coordinates = getRandomCoordinates(
+      randomNumber,
+      latitudeBoundaries,
+      longitudeBoundaries
+    );
+
+    res.send(coordinates);
+    console.log(coordinates);
   }
 );
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 // helper functions
+const calculateRandomFloatFromArray = array =>
+  Math.random() * (array[0] - array[1]) - array[1];
+
+const createLatitudeLongitudeArrays = jsonData => {
+  // separates coordinates into latitude and longitude arrays
+  let latitudeArray = [];
+  let longitudeArray = [];
+  for (let jsonArray of Object.values(jsonData)) {
+    latitudeArray.push(parseFloat(jsonArray[0]));
+    longitudeArray.push(parseFloat(jsonArray[1]));
+  }
+
+  return [latitudeArray, longitudeArray];
+};
+
+const getRandomCoordinates = (
+  coordinateAmount,
+  latitudeBoundaries,
+  longitudeBoundaries
+) => {
+  // return given number of coordinates within given boundaries.
+  const coordinates = [];
+  const name = 'coordinate';
+  const DECIMAL_PLACES = 4;
+
+  // Create random coordinates
+  for (let i = 0; i < coordinateAmount; i++) {
+    const coordinate = {};
+    const coordinateData = [];
+    const key = name + i;
+
+    // Calculate randomness
+    const latitude = calculateRandomFloatFromArray(latitudeBoundaries);
+    const longitude = calculateRandomFloatFromArray(longitudeBoundaries);
+
+    // build coordinate array
+    coordinateData.push(latitude.toFixed(DECIMAL_PLACES));
+    coordinateData.push(longitude.toFixed(DECIMAL_PLACES));
+
+    coordinate[key] = coordinateData;
+    // add named array to coordinates dictionary
+    coordinates.push(coordinate);
+  }
+  return coordinates;
+};
+
 const getRandomNumber = (ceiling, offset) =>
   Math.floor(Math.random() * ceiling) + offset;
+
+module.exports = createLatitudeLongitudeArrays;
