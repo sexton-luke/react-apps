e1a9645562cdda5a124d56454cf20b058d8a20e1
lukej <31764152+sexton-luke@users.noreply.github.com>
2022-04-10 00:02:59 +1000

    add css styling, custom markers, popups

--- plot-box/src/components/map/functions/setMarkers.js
+++ plot-box/src/components/map/functions/setMarkers.js
@@ -15,11 +15,24 @@ const setMarkers = (map, coordinates, markers) => {
   console.log('markers: ', markers);
 
 
+
+
   markers = clearMarkers(map, markers);
 
   for (let coordinate of Object.keys(coordinates)) {
-    const marker = new mapboxgl.Marker()
+    const element = document.createElement('div');
+    element.className = 'marker';
+
+    const marker = new mapboxgl.Marker(element)
       .setLngLat(coordinates[coordinate])
+      .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+          .setHTML(
+            `<h3>Lng Lat <br></h3>
+            <p>${coordinates[coordinate]['lng']} |
+            ${coordinates[coordinate]['lat']}</p>`
+          )
+      )
       .addTo(map);
 
     markers.push(marker);
